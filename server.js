8e7fdaee74f93ceb4c6805b1bac20577600d4efc
MistrBug <mistrbug@gmail.com>
2026-09-23 22:09:07 +0100

    Corrigir layout da página /cv e atualizar link Ver Currículo

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.get('/health', (req, res) => {
 });
 
 // Página 404 para rotas inexistentes
+// Rota para a página de currículo
+app.get('/cv', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'cv.html'));
+});
+
+// Página 404 para rotas inexistentes
 app.use((req, res) => {
   res.status(404).send('<h1>404 – Página não encontrada</h1>');
 });
